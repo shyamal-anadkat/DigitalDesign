448984750738ad319e66dc4927667330c8d650e6
Shyamal Anadkat <shyamalanadkat@gmail.com>
2017-10-28 19:56:31 -0500

    ex14 passing test

--- Exercise14 (Designing  Testing Flight Control Math)-20171026/flght_cntrl_skeleton.sv
+++ Exercise14 (Designing  Testing Flight Control Math)-20171026/flght_cntrl_skeleton.sv
@@ -30,13 +30,15 @@ reg signed [9:0] prev_yaw_err[0:D_QUEUE_DEPTH-1];	// need previous error terms f
 // You will need a bunch of interal wires declared //
 // for intermediate math results...do that here   //
 ///////////////////////////////////////////////////
-logic [16:0] ptch_err, yaw_err, roll_err;
-logic [9:0]  ptch_err_sat, yaw_err_sat, roll_err_sat;
-logic [9:0]  ptch_D_diff, roll_D_diff, yaw_D_diff;
-logic signed [9:0] ptch_pterm, yaw_pterm, roll_pterm;
-logic signed [11:0] ptch_dterm, yaw_dterm, roll_dterm;
+wire signed [16:0] ptch_err, yaw_err, roll_err;
+wire signed [9:0]  ptch_err_sat, yaw_err_sat, roll_err_sat;
+wire signed [9:0]  ptch_D_diff, roll_D_diff, yaw_D_diff;
+wire signed [9:0] ptch_pterm, yaw_pterm, roll_pterm;
+wire signed [11:0] ptch_dterm, yaw_dterm, roll_dterm;
 
-logic signed [5:0] ptch_D_diff_sat, roll_D_diff_sat, yaw_D_diff_sat;
+wire signed [5:0] ptch_D_diff_sat, roll_D_diff_sat, yaw_D_diff_sat;
+wire [12:0] frnt_spd_sum, back_spd_sum, left_spd_sum, right_spd_sum;
+wire [10:0] frnt_spd_sat, back_spd_sat, left_spd_sat, right_spd_sat;
 
 ///////////////////////////////////////////////////////////////
 // some Parameters to keep things more generic and flexible //
@@ -44,73 +46,135 @@ logic signed [5:0] ptch_D_diff_sat, roll_D_diff_sat, yaw_D_diff_sat;
 
   localparam CAL_SPEED = 11'h1B0;		// speed to run motors at during inertial calibration
   localparam MIN_RUN_SPEED = 13'h200;	// minimum speed while running  
-  localparam D_COEFF = 6'b00111;			// D coefficient in PID control = +7
-  localparam MAX_10 = 10'b0111111111;
-  localparam MIN_10 = 10'b1000000000;
-  localparam MAX_6 = 6'b011111;
-  localparam MIN_6 = 6'b100000;
+  localparam D_COEFF = 6'b00111;		// D coefficient in PID control = +7
+  
+  localparam MAX_10 = 10'h1FF;
+  localparam MIN_10 = 10'h200;
+  localparam MAX_6 = 6'h1F;
+  localparam MIN_6 = 6'h20;
+  localparam MAX_11 = 11'h7FF;
   
 /// OK...rest is up to you...good luck! ///
+
+//ptch, roll, yaw err calculations 
 assign ptch_err = ptch - d_ptch;
-assign roll_err = roll - d_roll;
-assign yaw_err =  yaw -  d_yaw;
 
 //signed-err-sat 17 to 10
-assign ptch_err_sat = (~ptch_err[16]) ?           //positive ? 
-(!(|ptch_err[16:10]) ? ptch_err[9:0] : MAX_10) :  //is any bit in 16:10 unset ? 
-((&ptch_err[16:10]) ? ptch_err[9:0]: MIN_10);
+assign ptch_err_sat = (ptch_err[16]) ?           
+((&ptch_err[15:9]) ? ptch_err[9:0] : MIN_10) :  
+((|ptch_err[15:9]) ? MAX_10: ptch_err[9:0]);
 
-assign roll_err_sat = (~roll_err[16]) ?           //positive ? 
-(!(|roll_err[16:10]) ? roll_err[9:0] : MAX_10) :  //is any bit in 16:10 unset ? 
-((&roll_err[16:10]) ? roll_err[9:0]: MIN_10);
-
-assign yaw_err_sat = (~yaw_err[16]) ?           //positive ? 
-(!(|yaw_err[16:10]) ? yaw_err[9:0] : MAX_10) :  //is any bit in 16:10 unset ? 
-((&yaw_err[16:10]) ? yaw_err[9:0]: MIN_10);
+assign roll_err = roll - d_roll;
+assign roll_err_sat = (roll_err[16]) ?           
+((&roll_err[15:9]) ? roll_err[9:0] : MIN_10) :  
+((|roll_err[15:9]) ? MAX_10: roll_err[9:0]);
 
 
+assign yaw_err =  yaw -  d_yaw;
+assign yaw_err_sat = (yaw_err[16]) ?           
+((&yaw_err[15:9]) ? yaw_err[9:0] : MIN_10) :  
+((|yaw_err[15:9]) ? MAX_10: yaw_err[9:0]);
+
+//Shift on vld
+always_ff @(posedge clk, negedge rst_n)
+if (!rst_n)
+for (x = 0; x < D_QUEUE_DEPTH; x=x+1) begin
+	prev_ptch_err[x] = 10'h000;
+	prev_roll_err[x] = 10'h000;
+	prev_yaw_err[x] = 10'h000;
+end
+else if (vld) begin
+	for (x = D_QUEUE_DEPTH-1; x > 0; x=x-1) begin
+		prev_ptch_err[x] = prev_ptch_err[x - 1];
+		prev_roll_err[x] = prev_roll_err[x - 1];
+		prev_yaw_err[x] = prev_yaw_err[x - 1];
+	end
+	prev_ptch_err[0] = ptch_err_sat;
+	prev_roll_err[0] = roll_err_sat;
+	prev_yaw_err[0] = yaw_err_sat;
+end
+
+//PTCH
+assign ptch_D_diff = prev_ptch_err[0] - prev_ptch_err[D_QUEUE_DEPTH-1];
 //saturate ptch_D_Diff [9:0] 10 to 6 bits 
-assign ptch_D_diff_sat = (~ptch_D_Diff[9]) ?           
-(!(|ptch_D_[9:6]) ? ptch_D_Diff[5:0] : MAX_6) :     
-((&ptch_D_diff[9:6]) ? ptch_D_Diff[5:0]: MIN_6);
-
-assign roll_D_diff_sat = (~roll_D_Diff[9]) ?           
-(!(|roll_D_[9:6]) ? roll_D_Diff[5:0] : MAX_6) :     
-((&roll_D_diff[9:6]) ? roll_D_Diff[5:0]: MIN_6);
-
-assign yaw_D_diff_sat = (~yaw_D_Diff[9]) ?           
-(!(|yaw_D_[9:6]) ? yaw_D_Diff[5:0] : MAX_6) :     
-((&yaw_D_diff[9:6]) ? yaw_D_Diff[5:0]: MIN_6);
-
-
-//ptch_pterm = (5/8) * ptch_err_sat OR [1/2(p) + 1/8(p)]
-assign ptch_pterm = (ptch_err_sat >>> 1) + (ptch_err_sat >>> 3); 
-assign roll_pterm = (roll_err_sat >>> 1) + (roll_err_sat >>> 3); 
-assign yaw_pterm =  (yaw_err_sat >>> 1) +  (yaw_err_sat >>> 3); 
-
-assign ptch_D_diff = ptch_err_sat - prev_ptch_err[D_QUEUE_DEPTH-1];
-assign roll_D_diff = roll_err_sat - prev_roll_err[D_QUEUE_DEPTH-1];
-assign yaw_D_diff  = yaw_err_sat -  prev_yaw_err[D_QUEUE_DEPTH-1];
-
+assign ptch_D_diff_sat = (ptch_D_diff[9]) ?           
+((&ptch_D_diff[8:6]) ? ptch_D_diff[5:0] : MIN_6) :     
+((|ptch_D_diff[8:6]) ? MAX_6 : ptch_D_diff[5:0]);
 //signed multiply
-assign ptch_dterm = $signed(D_COEFF) * ptch_D_diff_sat;
-assign roll_dterm = $signed(D_COEFF) * roll_D_diff_sat;
-assign yaw_dterm =  $signed(D_COEFF) *  yaw_D_diff_sat;
+assign ptch_dterm = ($signed(D_COEFF) * ptch_D_diff_sat);
+//ptch_pterm = (5/8) * ptch_err_sat OR [1/2(p) + 1/8(p)]
+assign ptch_pterm = ((ptch_err_sat >>> 1) + (ptch_err_sat >>> 3)); 
 
-always_ff @(posedge clk or negedge rst_n) begin 
-	if(!rst_n) begin
-		for(x = 0; x < D_QUEUE_DEPTH; x = x + 1) begin
-			prev_ptch_err[x] = 10'h000;
-			prev_roll_err[x] = 10'h000;
-			prev_yaw_err[x] = 10'h000;
-		end else if(vld) begin
-			for(x = D_QUEUE_DEPTH-1; x > 0; x = x+1) begin
-				prev_ptch_err[x] = prev_ptch_err[x-1];
-				prev_roll_err[x] = prev_roll_err[x-1];
-				prev_yaw_err[x] = prev_yaw_err[x-1];
-			end
-			prev_ptch_err[0] = ptch_err_sat;
-			prev_roll_err[0] = roll_err_sat;
-			prev_yaw_err[0] = yaw_err_sat;
-		end
-	endmodule 
+
+//ROLL
+assign roll_D_diff = prev_roll_err[0] - prev_roll_err[D_QUEUE_DEPTH-1];
+assign roll_D_diff_sat = (roll_D_diff[9]) ?           
+((&roll_D_diff[8:6]) ? roll_D_diff[5:0] : MIN_6) :     
+((|roll_D_diff[8:6]) ? MAX_6 : roll_D_diff[5:0]);
+
+assign roll_dterm = ($signed(D_COEFF) * roll_D_diff_sat);
+assign roll_pterm = ((roll_err_sat >>> 1) + (roll_err_sat >>> 3)); 
+
+//YAW
+assign yaw_D_diff  = prev_yaw_err[0] - prev_yaw_err[D_QUEUE_DEPTH-1];
+
+assign yaw_D_diff_sat = (yaw_D_diff[9]) ?           
+((&yaw_D_diff[8:6]) ? yaw_D_diff[5:0] : MIN_6) :     
+((|yaw_D_diff[8:6]) ? MAX_6 : yaw_D_diff[5:0]);
+
+assign yaw_dterm =  ($signed(D_COEFF) * yaw_D_diff_sat);
+assign yaw_pterm =  ((yaw_err_sat >>> 1) +  (yaw_err_sat >>> 3)); 
+
+
+
+//Front_speed = thrst + MIN_RUN_SPEED – ptch_Pterm – ptch_Dterm – yaw_Pterm – yaw_Dterm
+assign frnt_spd_sum = 
+MIN_RUN_SPEED + 
+{4'h0, thrst} - 
+{{3{ptch_pterm[9]}}, ptch_pterm} -
+{ptch_dterm[11], ptch_dterm} -
+{{3{yaw_pterm[9]}}, yaw_pterm} -
+{yaw_dterm[11], yaw_dterm};
+
+//Back_speed = thrst + MIN_RUN_SPEED + ptch_Pterm + ptch_Dterm – yaw_Pterm – yaw_Dterm
+assign back_spd_sum =
+MIN_RUN_SPEED + 
+{4'h0, thrst} +
+{{3{ptch_pterm[9]}}, ptch_pterm} +
+{ptch_dterm[11], ptch_dterm} -
+{{3{yaw_pterm[9]}}, yaw_pterm} -
+{yaw_dterm[11], yaw_dterm};
+
+
+//Left_speed = thrst + MIN_RUN_SPEED - roll_Pterm - roll_Dterm + yaw_Pterm + yaw_Dterm
+assign left_spd_sum = 
+MIN_RUN_SPEED + 
+{4'h0, thrst} -
+{{3{roll_pterm[9]}}, roll_pterm} -
+{roll_dterm[11], roll_dterm} +
+{{3{yaw_pterm[9]}}, yaw_pterm} +
+{yaw_dterm[11], yaw_dterm};
+
+//Right_speed = thrst + MIN_RUN_SPEED + roll_Pterm + roll_Dterm + yaw_Pterm + yaw_Dterm
+assign right_spd_sum = 
+MIN_RUN_SPEED + 
+{4'h0, thrst} +
+{{3{roll_pterm[9]}}, roll_pterm} +
+{roll_dterm[11], roll_dterm} +
+{{3{yaw_pterm[9]}}, yaw_pterm} +
+{yaw_dterm[11], yaw_dterm};
+
+//saturate 13 to 11 bits (unsigned)
+
+assign frnt_spd_sat  = (|frnt_spd_sum[12:11]) ? MAX_11 : frnt_spd_sum[10:0];	
+assign back_spd_sat  = (|back_spd_sum[12:11]) ? MAX_11 : back_spd_sum[10:0];	
+assign left_spd_sat  = (|left_spd_sum[12:11]) ? MAX_11 : left_spd_sum[10:0];	
+assign right_spd_sat = (|right_spd_sum[12:11]) ? MAX_11 : right_spd_sum[10:0];	
+
+//calibration 
+assign frnt_spd = inertial_cal ? CAL_SPEED : frnt_spd_sat;
+assign bck_spd = inertial_cal ? CAL_SPEED : back_spd_sat;
+assign lft_spd = inertial_cal ? CAL_SPEED : left_spd_sat;
+assign rght_spd = inertial_cal ? CAL_SPEED : right_spd_sat;
+
+endmodule // flght_cntrl
